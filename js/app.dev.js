0c9f1ccb266ad63e015e8dc3b370bef1257bda60
PineappleRind <pineapplerind.info@gmail.com>
2021-02-02 15:21:31 -0500

    ppush

--- js/app.dev.js
+++ js/app.dev.js
@@ -1,43 +1,33 @@
 "use strict";
 
-function $(id) {
-    return document.querySelector(id)
+function $(e) {
+    return document.querySelector(e)
 }
-
-let o = $('#overlay');
-
-onload = () => {
-    o.style.opacity = '0'
-    setTimeout(function(){
-        o.style.display = 'none';
-    },200)
-}
-let r = $("#mobileNav");
-function openNav() {
-    r.classList.toggle("is-open");
-    r.classList.toggle("is-closed");
-}
-
-function openOverlay(o) {
-    setTimeout(() => {
-        o.style.opacity = '0.8';
-    },10)
-    o.style.display = 'block'
-}
-
-function closeOverlay(o) {
-    setTimeout(() => {
-        o.style.display = 'none'
-    },200)
-    o.style.opacity = '0';
-}
- 
-$('.menu').addEventListener("click", function(e) {
-    openNav();
-    openOverlay($('#navOverlay'));
+let o = $("#overlay");
+onload = (() => {
+    o.style.opacity = "0", setTimeout(function() {
+        o.style.display = "none"
+    }, 200)
 });
+let r = $("#mobileNav");
 
-$('#close').addEventListener("click", function(e) {
-    openNav();
-    closeOverlay($('#navOverlay'));
+function openNav() {
+    r.classList.toggle("is-open"), r.classList.toggle("is-closed")
+}
+
+function openOverlay(e) {
+    setTimeout(() => {
+        e.style.opacity = "0.8"
+    }, 10), e.style.display = "block"
+}
+
+function closeOverlay(e) {
+    setTimeout(() => {
+        e.style.display = "none"
+    }, 700), e.style.opacity = "0"
+}
+$(".menu").addEventListener("click", function(e) {
+    openNav(), openOverlay($("#navOverlay"))
+}), $("#close").addEventListener("click", function(e) {
+    openNav(), closeOverlay($("#navOverlay"))
 });
