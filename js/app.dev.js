7c6fadbf46e0f1803f434d06ddcaf6977e69fa8f
PineappleRind <pineapplerind.info@gmail.com>
2021-02-02 12:45:45 -0500

    naaavaav

--- js/app.dev.js
+++ js/app.dev.js
@@ -10,14 +10,34 @@ onload = () => {
     o.style.opacity = '0'
     setTimeout(function(){
         o.style.display = 'none';
-    },400)
+    },200)
 }
 let r = $("#mobileNav");
 function openNav() {
     r.classList.toggle("is-open");
-    r.classList.toggle("is-closed")
+    r.classList.toggle("is-closed");
+}
+
+function openOverlay(o) {
+    setTimeout(() => {
+        o.style.opacity = '0.8';
+    },10)
+    o.style.display = 'block'
+}
+
+function closeOverlay(o) {
+    setTimeout(() => {
+        o.style.display = 'none'
+    },200)
+    o.style.opacity = '0';
 }
  
 $('.menu').addEventListener("click", function(e) {
     openNav();
+    openOverlay($('#navOverlay'));
+});
+
+$('#close').addEventListener("click", function(e) {
+    openNav();
+    closeOverlay($('#navOverlay'));
 });
